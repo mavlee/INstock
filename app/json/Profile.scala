7fd75beffd8a6839bfdaccf57801c4c7d9923800
Maverick <maverick.lee225@gmail.com>
2012-05-18 14:08:45 -0700

    Clean up white space

--- app/json/Profile.scala
+++ app/json/Profile.scala
@@ -2,4 +2,4 @@ package json
 
 class Profile(var id:String,var firstName: String, var lastName: String,
     var headline:String, var summary:String, var pictureUrl:String, var industry:String)
-  
+
