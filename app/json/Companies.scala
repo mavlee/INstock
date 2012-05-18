4e0b16c89e289a05478d0915c02597efc4411960
Maverick <maverick.lee225@gmail.com>
2012-05-18 14:28:34 -0700

    Change Date to string in company

---
+++ app/json/Companies.scala
@@ -0,0 +1,5 @@
+package json
+
+class Companies(var _total:java.lang.Integer, var values:Array[Company])
+
+class Company(var name:String, var ticker:String, var startDate:String, var endDate:String)
